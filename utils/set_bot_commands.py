8755bb98264c497f72434ee9250feefc71991136
Valeriy <valeravoronov88@gmail.com>
2023-01-19 20:09:31 +0300

    работа над ошибками

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -3,6 +3,9 @@ from config_data.config import DEFAULT_COMMANDS
 
 
 def set_default_commands(bot):
+    """
+    Функция выводит в меню список команд с описанием действия
+    """
     bot.set_my_commands(
         [BotCommand(*i) for i in DEFAULT_COMMANDS]
     )
