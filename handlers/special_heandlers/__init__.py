7ecc16cec2ec26f0a1c3d2a7d704f1be80f30b18
Valeriy <valeravoronov88@gmail.com>
2023-04-07 16:42:33 +0300

    добавлены import для перехода в скрипты

--- handlers/special_heandlers/__init__.py
+++ handlers/special_heandlers/__init__.py
@@ -1,9 +1,8 @@
-# Когда ввожу любую команду программа начинает выполняться с первого найденного файла, а не с той команды которую я ввел
-# к примеру я ввожу команду /high но код будет выполняться команды custom т.к. она стоит выше команды high так же и с /low
-# не понимаю как это исправить
 from . import history
 from . import custom
 from . import high
 from . import low
+from . import hotel_search
 
-__all__ = ['history', 'custom', 'high', 'low']
+
+__all__ = ['history', 'custom', 'high', 'low', 'hotel_search']
