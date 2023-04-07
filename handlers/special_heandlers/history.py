5bbebb15e00084db011269bea045797dffd7cc1a
Valeriy <valeravoronov88@gmail.com>
2023-04-07 16:33:21 +0300

    Добавлена функция по поиску истории запросов пользователя

--- handlers/special_heandlers/history.py
+++ handlers/special_heandlers/history.py
@@ -1,7 +1,13 @@
 from telebot.types import Message
+from database.case import database
 from loader import bot
 
 
 @bot.message_handler(commands=['history'])
-def send_welcome(message: Message):
-    bot.send_message(message.chat.id, 'history')
+def user_request_history(message: Message):
+    scroll_request_history = database.select_users(message.from_user.id)
+    for requests_user in scroll_request_history:
+        bot.send_message(message.chat.id, f'Дата запроса: {requests_user[4]}\n'
+                                          f'Команда: {requests_user[3]}\n'
+                                          f'Город: {requests_user[0]}\n'
+                                          f'Пребывание с {requests_user[1]} по {requests_user[2]}')
