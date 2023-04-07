b99bfd9a5eda651885820094e40a81ac7559b2e2
Valeriy <valeravoronov88@gmail.com>
2023-04-07 16:40:09 +0300

    Добавлены переменные для хранения значений введенных пользователем

--- states/answer_user.py
+++ states/answer_user.py
@@ -2,7 +2,20 @@ from telebot.handler_backends import State, StatesGroup
 
 
 class UserAnswerState(StatesGroup):
-    service = State()
+    city = State()
+    city_high = State()
+    city_low = State()
+    arrival_date = State()
+    arrival_date_high = State()
+    arrival_date_low = State()
+    departure_date = State()
+    departure_date_high = State()
+    departure_date_low = State()
     low_value = State()
+    low_value_low = State()
     high_value = State()
+    high_value_high = State()
     amount_product = State()
+    amount_product_low = State()
+    amount_product_high = State()
+    choice = State()
